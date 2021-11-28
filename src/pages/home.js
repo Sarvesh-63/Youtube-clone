0f32d814e63f6ebccf2145bd05e7b4e402108dc9
unknown <waransarvesh39@gmail.com>
2021-11-28 23:08:18 +0530

    first

---
+++ src/pages/home.js
@@ -0,0 +1,53 @@
+import CustomAppBar from "../components/CustomAppBar";
+import { useState, useEffect } from "react";
+import CustomTile from "../components/CustomTile";
+import { Row } from "react-bootstrap";
+const Home = () => {
+  const [videos, setVideos] = useState([]);
+  const [searchText, setSearchText] = useState("");
+  useEffect(() => {
+    async function getVideos() {
+      const response = await fetch("https://salty-savannah-61881.herokuapp.com/videos", {
+        method: "GET",
+      });
+      const data = await response.json();
+      setVideos(data);
+    }
+    getVideos();
+  }, []);
+  return (
+    <div>
+      <CustomAppBar
+        label="Search"
+        onSearch={(event) => {
+          setSearchText(event.target.value);
+        }}
+      />
+      {videos.length === 0 ? (
+        <div class="d-flex align-items-center justify-content-center" >
+         <div class = "spinner-grow" role = "status" > <span class = "sr-only"></span></div>
+         </div>
+      ) : (
+        <Row xs={1} md={3} className="g-4">
+          {videos
+            .filter((e) =>
+              searchText.length !== 0
+                ? e.title.toLowerCase().includes(searchText.toLowerCase())
+                : true
+            )
+            .map((e) => (
+              <CustomTile
+                title={e.title}
+                uploadedAgo={e.uploadedAgo}
+                uploadedBy={e.uploadedBy}
+                image={e.image}
+                views={e.views}
+              />
+            ))}
+        </Row>
+      )}
+    </div>
+  );
+};
+
+export default Home;
